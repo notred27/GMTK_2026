e4df4aff3c7839a487dbf5d7c3c72fa959d831d0
Michael Reidy <mikereidy95@gmail.com>
2026-07-25 23:49:47 -0400

    Adding 3d assets

--- gmtk-2026/player/Player.gd
+++ gmtk-2026/player/Player.gd
@@ -18,17 +18,70 @@ extends CharacterBody3D
 
 @export var push_speed: float = 2.0
 
+@export var mesh: Node3D
+@export var rotation_speed: float = 10.0
 
-func move_player():
+
+@onready var animation_player: AnimationPlayer = find_child("AnimationPlayer", true, false) as AnimationPlayer
+enum WalkState { IDLE, STARTING, WALKING, STOPPING }
+var walk_state: WalkState = WalkState.IDLE
+
+
+func _ready() -> void:
+	if animation_player:
+		animation_player.animation_finished.connect(_on_animation_finished)
+
+
+
+func move_player(delta: float) -> void:
 	var dir = Vector3.ZERO
-	var move_dir = Input.get_vector("backward","forward","left", "right")
+	var move_dir = Input.get_vector("backward", "forward", "left", "right")
 	dir.x = move_dir.x
 	dir.z = move_dir.y
 	dir.y -= gravity
 	velocity = dir * speed
+	_update_mesh_rotation(dir, delta)
+	_update_animation(move_dir)
+
+func _update_animation(move_dir: Vector2) -> void:
+	if animation_player == null:
+		return
+	var is_moving = move_dir.length() > 0.1
+
+	match walk_state:
+		WalkState.IDLE:
+			if is_moving:
+				walk_state = WalkState.STARTING
+				animation_player.play("walk_start")
+		WalkState.WALKING:
+			if not is_moving:
+				walk_state = WalkState.STOPPING
+				animation_player.play("walk_end")
+		WalkState.STOPPING:
+			if is_moving:
+				walk_state = WalkState.STARTING
+				animation_player.play("walk_start")
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	if String(anim_name) == "walk_start" and walk_state == WalkState.STARTING:
+		walk_state = WalkState.WALKING
+		animation_player.play("walk_turn")
+	elif String(anim_name) == "walk_end" and walk_state == WalkState.STOPPING:
+		walk_state = WalkState.IDLE
+		#animation_player.play(idle_animation)
+
+func _update_mesh_rotation(dir: Vector3, delta: float) -> void:
+	if mesh == null:
+		return
+	var horizontal_dir = Vector3(dir.x, 0, dir.z)
+	if horizontal_dir.length() > 0.01:
+		var target_angle = atan2(horizontal_dir.x, horizontal_dir.z)
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, target_angle, rotation_speed * delta)
+
 
 func _physics_process(delta: float) -> void:
-	move_player()
+	move_player(delta)
 	move_and_slide()
 	push_rigid_bodies()
 	drain_energy(delta)
